0240568a98bbca4ae7e05c6df33b05b5c9541f38
Student <jscy@C25R203-14.campus.ad.bgu.ac.il>
2019-10-10 13:42:12 +0300

    dd data`

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -1,60 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node{
-
-	struct node * next;
+struct node {
 	int data;
+	struct node* next;
 };
-struct node*head;//globel var 
-
-struct node*Insert(int x) {
+struct node*head;
+struct node*append(int x) {
 	struct node*temp = (struct node*)malloc(sizeof(struct node));
 	temp->data = x;
 	temp->next = head;
 	head = temp;
 	return head;
 }
-/*void tail_to_lingth(node*tail,int value){
-struct node*temp = head;
-int flag = 0;
-while (flag == 0) {
-if (temp->next == NULL) {
-temp->data = value;
-head->next = temp->next;
-flag = 1;
+void isertathead(int x) {
+	append(x);
 }
-else {
-temp = temp->next;
-}
-}
-
-while (temp != NULL) {
-printf("the number you add to the last of the struct is  %d ", temp->data);
-temp = temp->next;
-}
-//printf("\n");
-
-  getchar();
-
-  }*/
-int insert_between(int x, int y) {// x is the index , y is the value i wann add
+void insertatposition(int x, int position) {
 	struct node*temp = head;
 	int c = 0, flag = 0;
 	while (flag == 0) {
-		if (temp->next != NULL) {
+		if (temp != NULL) {
 			c++;
-			if (c == x) {
-				struct node*h = (struct node*)malloc(sizeof(struct node));
-				temp->next = h;
-				h->data = y;
-				h->next = temp->next->next;
+			if (c == position) {
+				struct node*newnode = (struct node*)malloc(sizeof(struct node));
+				node*prev_temp = temp;
+				temp = newnode;
+				temp->data = x;
+				temp->next = prev_temp;
+
 				flag = 1;
 			}
 		}
 		else temp = temp->next;
+	}head = temp;
+}
+void deleteatposition(int position) {
+	struct node*temp = head;
+	int c = 0;
+	while (temp->next != NULL) {
+		c++;
+		if (c == (position - 1)) {
+			temp->next = temp->next->next;
+		}
+		temp = temp->next;
 	}
-	getchar();
-	return y;
 }
 void print() {
 	struct node*temp = head;
@@ -63,24 +52,25 @@ void print() {
 		temp = temp->next;
 	}
 	printf("\n");
+	getchar();
 }
+void reverse() {}
 void main() {
 	head = NULL;
-
-	Insert(3);
-	print();
-	int i, num;
+	int i, num, x;
 	printf("how many nodes ?\n");
-	scanf("%d", &num);
+	scanf_s("%d", &num);
 	for (i = 0; i < num; i++) {
 		printf(" enter the number\n");
-		scanf("%d", &head->data);
+		scanf_s("%d", &x);
+		append(x);
 	}
-	//printf(" enter a number you wanna add to the last of the struct  \n ");
-	//scanf_s("%d", &num2);
-	//tail_to_lingth(head,num2);
-	insert_between(5, 8);
-
+	isertathead(10);
+	insertatposition(5, 1);
+	print();
+	deleteatposition(2);
+	print();
 	getchar();
 
+}
 
